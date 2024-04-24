79060a17525312afb54e1ddf6660035b0e01d634
Y  Dayananda <dayanandaredd7993163@gmail.com>
2024-04-24 18:47:27 +0530

    Added answer_details model

---
+++ src/main/java/com/example/quara/models/AnswerDetails.java
@@ -0,0 +1,26 @@
+package com.example.quara.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerDetails extends BaseModel {
+
+
+    private Long upvote;
+
+    private Long downvote;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Answer answer = new Answer();
+
+}
